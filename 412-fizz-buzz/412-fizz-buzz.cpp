cb1cc09f22f888b942cc3617270591a46a1c9495
Ahmed Muhammad Sayed <35504456+smara97@users.noreply.github.com>
2022-09-06 11:24:52 +0200

    Time: 15 ms (6.22%), Space: 7.2 MB (97.17%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        
+        vector<string>vs(n);
+        
+        stringstream stream;
+        
+        for(int i=1;i<=n;i++){
+            
+            if((i%3) == 0 && (i%5) == 0 )
+                vs[i-1] = "FizzBuzz";
+            else if((i%3) == 0)
+                vs[i-1] = "Fizz";
+            else if((i%5) == 0)
+                vs[i-1] = "Buzz";
+            else{
+                stringstream stream;
+                stream << i;
+                stream >> vs[i-1];
+            }
+        }
+        
+        return vs;
+    }
+};
